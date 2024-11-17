b0f9b99f493d3270b91c7c9c42b57c51d7b59605
Andrei-Constantin <andrei-constantin.popa@estudiantat.upc.edu>
2024-11-17 17:32:34 +0100

    intialize + material ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent],
+  template: `<main>
+    <header class="brand-name">
+      <h1>Hello</h1>
+    </header>
+    <section>
+      <app-home></app-home>
+    </section>
+  </main>`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'nemi-test';
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `<div>home</div>`,
+  styleUrl: './home.component.css',
+})
+export class HomeComponent {}
